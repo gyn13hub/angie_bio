1702e041b1fc0317726ed80a04eeb121f504c2db
gyn13hub <261350973+gyn13hub@users.noreply.github.com>
2026-06-09 08:38:09 +0900

    Update sample.ts

--- src/sample.ts
+++ src/sample.ts
@@ -4,6 +4,7 @@ const author = {
   facebook: "https://www.facebook.com/gyn13here",
   discord: "https://discord.com/invite/5vVV9qHtJ8",
   linkedin: "https://www.linkedin.com/in/huu-nguyen-5028713b0",
+  line: "https://line.me/ti/p/bm_NantMaT",
 };
 
 const buy = {
